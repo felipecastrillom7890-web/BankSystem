22ba99ff378afd9d207e9703e9f83b6cc942a06d
felipecastrillom7890-web <felipecastrillom7890@gmail.com>
2026-07-27 11:25:59 -0500

    Se elimina modulo de cuentas y se deja CRUD de clientes

--- src/main.py
+++ src/main.py
@@ -1,26 +1,21 @@
 from controllers.cliente_controller import ClienteController
-from controllers.cuenta_controller import CuentaController
 
 
 def menu():
 
     cliente_controller = ClienteController()
-    cuenta_controller = CuentaController()
 
     while True:
 
         print("\n========== SISTEMA BANCARIO ==========")
         print("1. Registrar cliente")
         print("2. Listar clientes")
-        print("3. Crear cuenta")
-        print("4. Listar cuentas")
-        print("5. Buscar cuenta")
-        print("6. Consignar dinero")
-        print("7. Retirar dinero")
-        print("8. Salir")
+        print("3. Buscar cliente")
+        print("4. Actualizar cliente")
+        print("5. Eliminar cliente")
+        print("6. Salir")
 
         opcion = input("\nSeleccione una opción: ")
-
 
         if opcion == "1":
             cliente_controller.registrar_cliente()
@@ -29,27 +24,20 @@ def menu():
             cliente_controller.listar_clientes()
 
         elif opcion == "3":
-            cuenta_controller.crear_cuenta()
+            cliente_controller.buscar_cliente()
 
         elif opcion == "4":
-            cuenta_controller.listar_cuentas()
+            cliente_controller.actualizar_cliente()
 
         elif opcion == "5":
-            cuenta_controller.buscar_cuenta()
+            cliente_controller.eliminar_cliente()
 
         elif opcion == "6":
-            cuenta_controller.consignar_dinero()
-
-        elif opcion == "7":
-            cuenta_controller.retirar_dinero()
-
-        elif opcion == "8":
             print("\nGracias por usar el sistema.")
             break
 
         else:
             print("\n❌ Opción no válida.")
-
 
 
 if __name__ == "__main__":
